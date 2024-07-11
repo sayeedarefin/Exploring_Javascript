bc98f367cf17d9bd6ba2cee08b82b2c48dc33a01
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-11 16:01:56 +0600

    solutions

---
+++ practice_problem.js
@@ -0,0 +1,12 @@
+// solution-2
+
+var math = 75.25;
+var physics = 65;
+var chemistry = 80;
+var bio = 35.45;
+var ict = 99.50;
+var total = (math+physics+chemistry+bio+ict);
+var subs = 5;
+var avg = (total/subs);
+avg = avg.toFixed(2)
+console.log(avg);

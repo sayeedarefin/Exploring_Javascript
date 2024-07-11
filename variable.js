998c0c22c347ff44d5cc1940e1c1c49b4186e309
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-11 06:01:29 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,18 @@
+// you have to add 5 things to declare a variable
+
+
+//Numeric variable
+var chini=50;
+var tem = 38;
+var age =24;
+var ekta_number =500;
+
+//string variable: you have to put the value in qoutation mark
+var name= "Sayeed";
+var location= "lalkhan bazar";
+var occupation= "petroleum engineer";
+
+//boolean Variable
+var issingle = false;
+var ISpass = true;
+var eat = true;

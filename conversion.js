a9e7bdc087fa32bd515fa922694828175299657a
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-11 15:14:34 +0600

    learned conversion of string into numerics

---
+++ conversion.js
@@ -0,0 +1,20 @@
+var firtsName = "sayeed";
+var lastName = "arefin";
+var fullName = (firtsName + ' ' + lastName);
+// console.log(fullName);
+
+var price1 = '32';
+var price2 = 85;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+//now we want to convert that string into integer, just simplt add parseInt()
+
+var price1 = '32';
+var price2 = 85;
+var price1Number = parseInt(price1);
+// console.log(price1Number);
+
+var valueOfPi = 3.1416;
+var valueOfPiNumber = parseFloat(valueOfPi);
+console.log(valueOfPiNumber);

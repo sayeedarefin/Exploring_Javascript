7b52e267ee7daba4f76b2f90313a492abdf5f3d1
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-11 06:44:11 +0600

    3rd commit

---
+++ operation.js
@@ -0,0 +1,21 @@
+// variable declaration
+
+var onionPrice = 55;
+var eggPrice = 36;
+var onionQuantity = 28;
+var money = 5000;
+var player = 20;
+
+// operation variable name declaration
+
+var addition = (onionPrice + eggPrice);
+var substration = (onionPrice - eggPrice);
+var multiplication = (onionPrice * eggPrice);
+var division =(money / player);
+
+// printing final results
+
+console.log(addition);
+console.log(substration);
+console.log(multiplication);
+console.log(division);

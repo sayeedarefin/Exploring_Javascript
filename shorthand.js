95d2c9b46af5be382d66a894ca584f60750a66ee
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-11 07:06:16 +0600

    fourth commit

---
+++ shorthand.js
@@ -0,0 +1,29 @@
+//variable declaration
+
+var priceOne = 30;
+var priceTwo = 50;
+
+// updating values by bangla niyom
+
+priceOne = priceOne + 10;
+console.log(priceOne);
+
+//updating values by shorthand
+
+priceOne += 10;
+console.log(priceOne);
+
+priceOne -= 10;
+console.log(priceOne);
+
+priceOne *= 10;
+console.log(priceOne);
+
+priceOne /= 10;
+console.log(priceOne);
+
+priceOne++;
+console.log(priceOne);
+
+priceOne--;
+console.log(priceOne);
